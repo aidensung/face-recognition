5e8d1fc2fc0f9e79d84fe4e20b62fcad4f3cfacf
Ilhyo Sung <sih8189@gmail.com>
2020-06-03 19:34:04 -0400

    changed api url and fixed vulnerabilities

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <p
-          onClick={() => onRouteChange("signout")}
-          className="f3 link dim black underline pa3 pointer"
+          onClick={() => onRouteChange('signout')}
+          className='f3 link dim black underline pa3 pointer'
         >
           Sign Out
         </p>
@@ -14,16 +14,16 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     );
   } else {
     return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <p
-          onClick={() => onRouteChange("signin")}
-          className="f3 link dim black underline pa3 pointer"
+          onClick={() => onRouteChange('signin')}
+          className='f3 link dim black underline pa3 pointer'
         >
           Sign In
         </p>
         <p
-          onClick={() => onRouteChange("register")}
-          className="f3 link dim black underline pa3 pointer"
+          onClick={() => onRouteChange('register')}
+          className='f3 link dim black underline pa3 pointer'
         >
           Register
         </p>
